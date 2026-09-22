0f9af9141749917c81c4116ffbdad350fea5b91e
challanger2000 <challanger2000@gmx.de>
2026-09-22 04:52:42 +0200

    Refine Tape magnetic saturation curve

--- source/media_oversampling_live.h
+++ source/media_oversampling_live.h
@@ -2,6 +2,6 @@
 #include "oversampling.h"
 #include <cmath>
 namespace MixEngine {
-inline double processTapeOversampledCore(OversamplingEngine& e,int& cur,int factor,double x,double shape){if(cur!=factor){e.reset();cur=factor;}return e.process(x,factor,[&](double v){return std::tanh(v*shape)/shape;});}
+inline double processTapeOversampledCore(OversamplingEngine& e,int& cur,int factor,double x,double shape){if(cur!=factor){e.reset();cur=factor;}const double k=std::max(0.0,shape-1.0);return e.process(x,factor,[&](double v){const double vv=v*v;return v/std::sqrt(1.0+(1.35*k)*vv);});}
 inline double processVinylOversampledCore(OversamplingEngine& e,int& cur,int factor,double x,double drive){if(cur!=factor){e.reset();cur=factor;}return e.process(x,factor,[&](double v){return std::tanh(v*drive)/drive;});}
 }
