0c3faf38e3cbd7357f0667dfd9a0f3f01dd1c644
challanger2000 <challanger2000@gmx.de>
2026-09-23 02:42:15 +0200

    Expand tape state for V2 hysteresis and head bump

--- source/processor.h
+++ source/processor.h
@@ -75,7 +75,7 @@ private:
 #endif
     struct ConsoleChannelState { double dcX1=0.0,dcY1=0.0,lowMemory=0.0,noiseMemory=0.0; std::uint32_t noiseRng=0; };
     using TubeChannelState = TubeModelState;
-    struct TapeChannelState { double highMemory=0.0,lowMemory=0.0,wowPhase=0.0,flutterPhase=0.0,previousInput=0.0,hissMemory=0.0,compressionEnvelope=0.0; std::uint32_t noiseRng=0; };
+    struct TapeChannelState { TapeMagneticState magnetic{}; double highMemory=0.0,bumpFast=0.0,bumpSlow=0.0,wowPhase=0.0,flutterPhase=0.0,previousInput=0.0,hissMemory=0.0,compressionEnvelope=0.0; std::uint32_t noiseRng=0; };
     struct GlueChannelState { double envelope=0.0; };
     struct VinylChannelState { double highMemory=0.0,lowMemory=0.0,surfaceMemory=0.0,clickEnvelope=0.0,clickPolarity=1.0; std::uint32_t noiseRng=0; };
     using StereoChannelState = StereoFieldState;
