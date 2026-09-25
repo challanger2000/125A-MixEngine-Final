0c8e8496c1ba12971bef206b85df2de92c9872d6
challanger2000 <challanger2000@gmx.de>
2026-09-25 04:27:17 +0200

    Cover 384 kHz in V3 sample-rate QA

--- tests/sample_rate_matrix.cpp
+++ tests/sample_rate_matrix.cpp
@@ -88,7 +88,7 @@ double runOne(double sr,bool mixfx){
 int main(){
  try{
   bool ok=true;
-  for(double sr:{44100.0,48000.0,88200.0,96000.0,192000.0}){
+  for(double sr:{44100.0,48000.0,88200.0,96000.0,192000.0,384000.0}){
    for(bool mixfx:{false,true}){
     const double p32=runOne<float>(sr,mixfx);
     const double p64=runOne<double>(sr,mixfx);
